789d8ec3de6b8137cd6f021b7899a32e11dd0287
Audiodrome <lien.daniel.h@gmail.com>
2017-10-12 19:40:14 -0700

    [Update] Refactored seeding data for mongodb [Remove] Deleting collections using mongoose

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const faker = require('faker');
 const options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                   replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
-// const mongodbURI = 'mongodb://YOUR MONGODB URI';
+// const mongodbURI = 'mongodb://YOUR_MONGODB_URI';
 const mongodbURI = 'mongodb://127.0.0.1:27017/puptest';
 mongoose.connect(mongodbURI, {
   useMongoClient: true,
@@ -32,9 +32,11 @@ const dogSchema = new Schema({
 const ownerSchema = new Schema({
   _id: Schema.Types.ObjectId,
   name: String,
-  location: Number,
+  location: String,
   age: { type: Number, min: 18, max: 101},
+  picture: String,
   bio: String,
+  rating: Number,
   dogs: [{type: Schema.Types.ObjectId, ref: 'Dogs'}],
   dogsSeen: [{type: Schema.Types.ObjectId, ref: 'Dogs'}],
   dogsLiked: [{type: Schema.Types.ObjectId, ref: 'Dogs'}]
@@ -43,51 +45,64 @@ const ownerSchema = new Schema({
 const Owners = mongoose.model('Owners', ownerSchema);
 const Dogs = mongoose.model('Dogs', dogSchema);
 
-db.dropCollection('owners', (err) => {
-  if (err) {
-    console.log('owners collection did not delete', err);
-    return;
-  }
-  console.log('owners collection deleted');
-});
+// db.dropCollection('owners', (err) => {
+//   if (err) {
+//     console.log('owners collection did not delete', err);
+//     return;
+//   }
+//   console.log('owners collection deleted');
+// });
 
-db.dropCollection('dogs', (err) => {
-  if (err) {
-    seed();
-    console.log('dogs collection did not delete', err);
-    return;
-  }
-  seed();
-  console.log('dogs collection deleted');
-});
+// db.dropCollection('dogs', (err) => {
+//   if (err) {
+//     console.log('dogs collection did not delete', err);
+//     return;
+//   }
+//   seed();
+//   console.log('dogs collection deleted');
+// });
 
-let seed = function() {
-  for (let i = 0; i < 10; i++) {
-    let dog_id = new mongoose.Types.ObjectId();
-    let owner = new Owners({
-      _id: new mongoose.Types.ObjectId(),
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+for (let i = 0; i < 10; i++) {
+  let dog_id = new mongoose.Types.ObjectId();
+
+  let owner = new Owners({
+    _id: new mongoose.Types.ObjectId(),
+    name: faker.name.findName(),
+    age: getRandomInt(18, 101),
+    location: faker.address.zipCode(),
+    picture: faker.image.avatar(),
+    bio: faker.lorem.paragraph(),
+    rating: getRandomInt(1, 6),
+  });
+
+  owner.dogs.push(dog_id);
+
+  owner.save((err) => {
+    if (err) {
+      console.error('Could not save owner', err);
+    }
+
+    let dog = new Dogs({
+      _id: dog_id,
       name: faker.name.findName(),
-      age: Math.floor(Math.random() * 50) + 20,
+      owner: owner._id,
+      age: getRandomInt(0, 30),
     });
-    owner.dogs.push(dog_id);
-    owner.save((err) => {
+
+    dog.pictures.push(faker.image.cats());
+
+    dog.save((err) => {
       if (err) {
-        console.error('Could not save owner', err);
+        console.error('Could not save dog', err);
       }
-
-      let dog = new Dogs({
-        _id: dog_id,
-        name: faker.name.findName(),
-        owner: owner._id,
-        age: Math.floor(Math.random() * 25) + 1,
-      });
-
-      dog.save((err) => {
-        if (err) {
-          console.error('Could not save dog', err);
-        }
-      });
     });
-  }
-};
+  });
+}
+
 module.exports = db;
