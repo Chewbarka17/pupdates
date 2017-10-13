466ecd6a2fa16a74ae2bff86a8c462d259c0f03e
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-13 12:21:53 -0700

    [add] add dog works

--- server/controllers/dogController.js
+++ server/controllers/dogController.js
@@ -1,26 +1,52 @@
 // require dogs database
-const dogs = require('../../db/index');
+const db = require('../../db/index');
+const mongoose = require('mongoose');
+const owners = require('./userController');
+
+const mongodbURI = process.env.DB_URL;
+mongoose.connect(mongodbURI, {
+  useMongoClient: true,
+});
 
 module.exports = {
 
-  getAllDogs: (req, res) => {
-    // dogs.findAll
-  },
+  // getAllDogs: (req, res) => {
+  //   // dogs.findAll
+  // },
 
-  getDog: (req, res) => {
-    // get dog by id
-  },
+  // getDog: (req, res) => {
+  //   // get dog by id
+  // },
 
   addDog: (req, res) => {
-    // post dog to db
+    const dog = new db.Dogs({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      owner: req.body.oid,
+      age: req.body.age,
+      breed: req.body.breed,
+      pictures: req.body.pictures,
+    });
+    dog.save((err) => {
+      if (err) {
+        console.log('Failed to save dog ', err);
+      }
+    })
+      .then((data) => {
+        // update owners dogs array by owner id
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
   },
 
-  updateDog: (req, res) => {
-    // patch dog by dog id
-  },
+  // updateDog: (req, res) => {
+  //   // patch dog by dog id
+  // },
 
-  removeDog: (req, res) => {
-    // delete dog from db
-  }
+  // removeDog: (req, res) => {
+  //   // delete dog from db
+  // }
 
 };
