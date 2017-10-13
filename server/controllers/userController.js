3a05fd38abdd312e3d64c6080a7833c5d9dd3b1d
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-13 12:22:02 -0700

    [add] add user and it works

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,52 @@
 // require user database
-const users = require('../../db/index');
+const db = require('../../db');
+const mongoose = require('mongoose');
+
+const mongodbURI = process.env.DB_URL;
+mongoose.connect(mongodbURI, {
+  useMongoClient: true,
+});
 
 module.exports = {
 
-  getAllUsers: (req, res) => {
-    // users.findAll
-  },
+  // getAllUsers: (req, res) => {
+  //   // users.findAll
+  // },
 
-  getUser: (req, res) => {
-    // get user by id
-  },
+  // getUser: (req, res) => {
+  //   // get user by id
+  // },
 
   addUser: (req, res) => {
-    // post user to db
+    console.log(req.body);
+    const owner = new db.Owners({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      age: req.body.age,
+      location: req.body.location,
+      picture: req.body.picture,
+      bio: req.body.bio,
+      rating: req.body.rating,
+    });
+    owner.save((err) => {
+      if (err) {
+        console.error('Could not save owner', err);
+      }
+    })
+      .then((data) => {
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.status(500).send(err);
+      });
   },
 
-  updateUser: (req, res) => {
-    // patch user by user id
-  },
+  // updateUser: (req, res) => {
+  //   // patch user by user id
+  // },
 
-  removeUser: (req, res) => {
-    // delete user from db
-  }
+  // removeUser: (req, res) => {
+  //   // delete user from db
+  // }
 
 };
