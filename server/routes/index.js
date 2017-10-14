a2b39861ba77124e596a6f9bfa96a292fb89e0a9
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-13 19:31:06 -0700

    [add] routes for get all users and dogs

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,14 @@ const dogs = require('../controllers/dogController');
 const users = require('../controllers/userController');
 
 // dogs
-// router.get('/dogs', dogs.getAllDogs);
+router.get('/dogs', dogs.getAllDogs);
 // router.get('/dogs/:dogid', dogs.getDog);
 router.post('/dogs', dogs.addDog);
 // router.delete('/dogs', dogs.removeDog);
 router.patch('/dogs/:dogid', dogs.updateDog);
 
 // users
-// router.get('/users', users.getAllUser);
+router.get('/users', users.getAllUsers);
 // router.get('/users/:userid', users.getUser);
 router.post('/users', users.addUser);
 // router.delete('/users', users.removeUser);
