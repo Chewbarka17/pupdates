e9fc5a66b09fde7ac86b092b3c83344e6d352724
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-13 20:09:36 -0700

    [edit] uncomment delete routers

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,14 +8,14 @@ const users = require('../controllers/userController');
 router.get('/dogs', dogs.getAllDogs);
 router.get('/dogs/:dogid', dogs.getDog);
 router.post('/dogs', dogs.addDog);
-// router.delete('/dogs', dogs.removeDog);
+router.delete('/dogs/:dogid', dogs.removeDog);
 router.patch('/dogs/:dogid', dogs.updateDog);
 
 // users
 router.get('/users', users.getAllUsers);
 router.get('/users/:userid', users.getUser);
 router.post('/users', users.addUser);
-// router.delete('/users', users.removeUser);
+router.delete('/users/:userid', users.removeUser);
 router.patch('/users', users.updateUser);
 
 module.exports = router;
