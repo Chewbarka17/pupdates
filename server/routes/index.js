13806c8b7d52928d3971ec37637d0486cfe3a8ca
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-13 19:40:58 -0700

    [edit] uncomment get one dog and one user route

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,14 +6,14 @@ const users = require('../controllers/userController');
 
 // dogs
 router.get('/dogs', dogs.getAllDogs);
-// router.get('/dogs/:dogid', dogs.getDog);
+router.get('/dogs/:dogid', dogs.getDog);
 router.post('/dogs', dogs.addDog);
 // router.delete('/dogs', dogs.removeDog);
 router.patch('/dogs/:dogid', dogs.updateDog);
 
 // users
 router.get('/users', users.getAllUsers);
-// router.get('/users/:userid', users.getUser);
+router.get('/users/:userid', users.getUser);
 router.post('/users', users.addUser);
 // router.delete('/users', users.removeUser);
 router.patch('/users', users.updateUser);
