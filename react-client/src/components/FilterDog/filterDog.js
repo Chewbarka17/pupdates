6bdaeb623c701d421dec2c300a753cced0a04ac3
Tiffany Wang <twang421@gmail.com>
2017-10-17 16:47:33 -0700

    [CREATE] skeleton code for filterDog.js

---
+++ react-client/src/components/FilterDog/filterDog.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { StackNavigator} from 'react-navigation';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+class FilterScreen extends React.Component {
+    static navigationOptions = {
+      title: 'Filter',
+    };
+    render() {
+      const { navigate } = this.props.navigation;
+      return (
+        <View style={styles.container}>
+          <Text>
+            onPress= { ()=> navigate('Home') }>Navigate to Home
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  export default FilterScreen;
