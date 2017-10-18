9e5a150e887fa0a5f6fa90abc86d206da5281c8e
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-18 10:15:27 -0700

    [refactor]

--- react-client/src/components/Navbar/menu.js
+++ react-client/src/components/Navbar/menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import {
   Platform,
   StyleSheet,
@@ -58,13 +58,13 @@ class MenuScreen extends React.Component {
     }
   }
 
-  const NavigationApp = StackNavigator({
-    Menu: { screen: MenuScreen },
-    Dogs: { screen: ViewDogsScreen },
-    Likes: { screen : LikesScreen },
-    Filter: { screen : FilterScreen },
-    Profile: { screen : OwnerProfileScreen },
-    Logout: { screen: LogoutScreen }
-    });
+const NavigationApp = StackNavigator({
+  Menu: { screen: MenuScreen },
+  Dogs: { screen: ViewDogsScreen },
+  Likes: { screen : LikesScreen },
+  Filter: { screen : FilterScreen },
+  Profile: { screen : OwnerProfileScreen },
+  Logout: { screen: LogoutScreen }
+});
 
   export default NavigationApp;
