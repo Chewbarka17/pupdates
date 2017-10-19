be184a3263c3940af1e0ea8d4d951faa2f0c04d6
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-18 17:16:29 -0700

    [add] owner actions

---
+++ react-client/src/actions/Profiles/ownerActions.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const postOwners = (firstname, lastname, age, zipcode, bio) => (dispatch) => {
+  axios.post('http://localhost:8000/api/users', {
+    name: firstname + lastname,
+    age,
+    location: zipcode,
+    bio,
+    picture: 'google.com',
+    rating: 4,
+  })
+    .then((response) => {
+      dispatch({ type: 'POST_OWNER_FULFILLED', payload: response.data });
+    })
+    .catch((err) => {
+      dispatch({ type: 'POST_OWNER_REJECTED', payload: err });
+    });
+};
+
