46e449d3b84ac16eae5e037fd06d54511ec86968
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-19 10:45:29 -0700

    [draft] owner update actions

--- react-client/src/actions/Profiles/ownerActions.js
+++ react-client/src/actions/Profiles/ownerActions.js
@@ -17,3 +17,20 @@ export const postOwners = (firstname, lastname, age, zipcode, bio) => (dispatch)
     });
 };
 
+export const updateOwners = (firstname, lastname, age, zipcode, bio, userid) => (dispatch) => {
+  axios.patch(`http://localhost:8000/api/users/ + ${userid}`, {
+    name: `${firstname}  ${lastname}`,
+    age,
+    location: zipcode,
+    bio,
+    picture: 'google.com', // to be changed
+    rating: 4, // to be changed
+  })
+    .then((response) => {
+      dispatch({ type: 'POST_OWNER_FULFILLED', payload: response.data[1] });
+    })
+    .catch((err) => {
+      dispatch({ type: 'POST_OWNER_REJECTED', payload: err });
+    });
+};
+
