caf3a3ac58863ed92931918d894109702500a8fa
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-19 15:37:16 -0700

    [attempting] to navigate on click of button

--- react-client/src/components/Profiles/ownerProfile.js
+++ react-client/src/components/Profiles/ownerProfile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StackNavigator} from 'react-navigation';
+import { StackNavigator } from 'react-navigation';
 import {
   Platform,
   StyleSheet,
@@ -13,105 +13,95 @@ import { connect } from 'react-redux';
 
 import * as ownerActions from '../../actions/Profiles/ownerActions';
 
-class OwnerProfileScreen extends React.Component {
-    static navigationOptions = {
-      title: 'Profile',
-    };
-    constructor(props) {
-      super(props);
+class OwnerProfileScreen extends Component {
+  static navigationOptions = {
+    title: 'Profile',
+  };
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        firstname: '',
-        lastname: '',
-        age: '',
-        zipcode: '',
-      };
-
-      this.handleSubmit = this.handleSubmit.bind(this);
-
+    this.state = {
+      name: '',
+      age: '',
+      location: '',
+      bio: '',
     };
 
-    handleSubmit() {
-      const { firstname, lastname, age, zipcode, bio, actions } = this.state;
-      this.props.actions.postOwners(firstname, lastname, age, zipcode);
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-    render() {
-      const { navigate } = this.props.navigation;
-      return (
-        <View>
-          <FormLabel>First Name</FormLabel>
-          <FormInput
-            editable
-            autoCapitalize="none"
-            autoCorrect={false}
-            underlineColorAndroid="transparent"
-            placeholder="Enter your First Name"
-            returnKeyType="next"
-            id="firstname"
-            onChangeText={firstname => this.setState({ firstname })}
-          />
-          <FormLabel>Last Name</FormLabel>
-          <FormInput
-            editable
-            autoCapitalize="none"
-            autoCorrect={false}
-            underlineColorAndroid="transparent"
-            placeholder="Enter your Last Name"
-            returnKeyType="next"
-            id="lastname"
-            onChangeText={lastname => this.setState({ lastname })}
-          />
-          <FormLabel>Age</FormLabel>
-          <FormInput
-            editable
-            autoCapitalize="none"
-            autoCorrect={false}
-            underlineColorAndroid="transparent"
-            placeholder="Enter your Age"
-            returnKeyType="next"
-            id="age"
-            onChangeText={age => this.setState({ age })}
-          />
-          <FormLabel>Zipcode</FormLabel>
-          <FormInput
-            editable
-            autoCapitalize="none"
-            autoCorrect={false}
-            underlineColorAndroid="transparent"
-            placeholder="Enter your Zipcode"
-            returnKeyType="next"
-            id="zipcode"
-            onChangeText={zipcode => this.setState({ zipcode })}
-          />
-          <FormLabel>Bio</FormLabel>
-          <FormInput
-            editable
-            autoCapitalize="none"
-            autoCorrect={false}
-            underlineColorAndroid="transparent"
-            placeholder="Enter your Bio"
-            returnKeyType="next"
-            id="bio"
-            onChangeText={bio => this.setState({ bio })}
-          />
-          <Button
-            title="Save"
-            onPress={this.handleSubmit}
-            />
-        </View>
-      );
-    }
+  };
+
+  handleSubmit() {
+    const { name, age, location, bio, actions } = this.state;
+    this.props.actions.postOwners(name, age, location, bio)
+    navigate('viewOwnerProfile');
   }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <FormLabel>Name</FormLabel>
+        <FormInput
+          editable
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          placeholder={this.props.name || 'enter'}
+          returnKeyType="next"
+          id="name"
+          onChangeText={name => this.setState({ name })}
+        />
+        <FormLabel>Age</FormLabel>
+        <FormInput
+          editable
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          placeholder={this.props.age ? this.props.age.toString() : 'enter'}
+          returnKeyType="next"
+          id="age"
+          onChangeText={age => this.setState({ age })}
+        />
+        <FormLabel>Zipcode</FormLabel>
+        <FormInput
+          editable
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          placeholder={this.props.location || 'enter'}
+          returnKeyType="next"
+          id="zipcode"
+          onChangeText={location => this.setState({ location })}
+        />
+        <FormLabel>Bio</FormLabel>
+        <FormInput
+          editable
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+          placeholder={this.props.bio || 'enter'}
+          returnKeyType="next"
+          id="bio"
+          onChangeText={bio => this.setState({ bio })}
+        />
+        <Button
+          title="Save"
+          onPress={this.handleSubmit}
+          />
+      </View>
+    );
+  }
+}
 
   const ownerState = (store) => {
     return {
       //I don't think I need these... I'll eventually need users probably
-      // firstname: store.Owners.user.firstname,
-      // lastname: store.Owners.user.lastname,
-      // age: store.Owners.user.age,
-      // zipcode: store.Owners.user.zipcode,
-      // bio: store.Owners.user.bio
+      name: store.Owners.user.name,
+      age: store.Owners.user.age,
+      location: store.Owners.user.location,
+      bio: store.Owners.user.bio
+      // user: store.
     }
   }
 
