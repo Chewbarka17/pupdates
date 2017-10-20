53918dfb8f6879651a0caf21fec6dd0f6ed6de42
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-20 11:30:52 -0700

    [add] get request to dogs

--- react-client/src/components/Profiles/viewOwnerProfile.js
+++ react-client/src/components/Profiles/viewOwnerProfile.js
@@ -4,6 +4,9 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
+import axios from 'axios';
+
+import * as dogActions from '../../actions/Profiles/dogProfileActions';
 
 class viewOwnerProfile extends Component {
   static navigationOptions = {
@@ -12,20 +15,38 @@ class viewOwnerProfile extends Component {
   constructor(props) {
     super(props);
 
-    this.handlePress = this.handlePress.bind(this);
+    this.handlePressToEditUser = this.handlePressToEditUser.bind(this);
+    this.handlePressToAddDog = this.handlePressToAddDog.bind(this);
   }
 
-  handlePress() {
-    this.props.navigation.navigate('Profile')
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/users/dogs/59e8f89004abdcfd203864ef')
+      .then(({data}) => {
+        console.log(data);
+        })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
+  handlePressToEditUser() {
+    this.props.navigation.navigate('Profile');
+  }
+
+  handlePressToAddDog() {
+    this.props.navigation.navigate('EditDogProfile');
+  }
 
   render () {
     return (
       <View>
         <Button 
-        title='Edit'
-        onPress={this.handlePress}
+        title='Edit User'
+        onPress={this.handlePressToEditUser}
+        />
+        <Button 
+        title='Add Dog'
+        onPress={this.handlePressToAddDog}
         />
       </View>
     )
