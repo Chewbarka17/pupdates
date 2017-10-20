a58b163410210ea0f3a1dcd13c2218c43473a2a0
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-20 14:33:46 -0700

    [list] owners dogs from the store

--- react-client/src/components/Profiles/viewOwnerProfile.js
+++ react-client/src/components/Profiles/viewOwnerProfile.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { StackNavigator } from 'react-navigation';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { View, Text, FlatList } from 'react-native';
+import { Button, List, ListItem } from 'react-native-elements';
 import axios from 'axios';
 
 import * as dogActions from '../../actions/Profiles/dogProfileActions';
@@ -14,30 +14,30 @@ class viewOwnerProfile extends Component {
   };
   constructor(props) {
     super(props);
-
     this.handlePressToEditUser = this.handlePressToEditUser.bind(this);
     this.handlePressToAddDog = this.handlePressToAddDog.bind(this);
   }
-
+  
   componentDidMount() {
     axios.get('http://localhost:8000/api/users/dogs/59e8f89004abdcfd203864ef')
-      .then(({data}) => {
-        console.log(data);
-        })
-      .catch((err) => {
-        console.log(err)
-      })
+    .then(({data}) => {
+      this.props.actions.listDogs(data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   }
-
+  
   handlePressToEditUser() {
     this.props.navigation.navigate('Profile');
   }
-
+  
   handlePressToAddDog() {
     this.props.navigation.navigate('EditDogProfile');
   }
-
+  
   render () {
+    console.log(this.props);
     return (
       <View>
         <Button 
@@ -48,9 +48,34 @@ class viewOwnerProfile extends Component {
         title='Add Dog'
         onPress={this.handlePressToAddDog}
         />
+        <FlatList
+          data={this.props.dogs}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.name}
+              subtitle={`Age: ${item.age} Breed: ${item.breed}`}
+            />
+          )}
+        />
       </View>
     )
   }
 }
 
-export default viewOwnerProfile;
+const dogsState = (store) => {
+  return {
+    dogs: store.Dogs.dogInfo
+  }
+}
+
+const dogDispatch = (dispatch) => {
+  return {
+    actions: bindActionCreators(dogActions, dispatch),
+  }
+};
+
+export default connect(dogsState, dogDispatch)(viewOwnerProfile);
+
+// {data.map((dog) => {
+//   <viewDogProfile/>
+// })}
