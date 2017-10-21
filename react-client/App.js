0f00572cefce43174efbb67cf1030fb005258dd1
Tiffany Wang <twang421@gmail.com>
2017-10-21 10:23:19 -0700

    [REMOVE] commented out code and import splash page file

--- react-client/App.js
+++ react-client/App.js
@@ -10,9 +10,16 @@ import {
 } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Store/Store';
+
+
 // import files
 // import NavigationApp from './src/components/NavBar/menu';
 import Navigator from './src/components/Navbar/menu';
+
+// this will replace <Navigator />
+import SplashPageView from './src/components/Splash/splashPageView';
+//<SplashPageView />
+
 
 export default class App extends Component<{}> {
   render() {
@@ -23,22 +30,3 @@ export default class App extends Component<{}> {
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
