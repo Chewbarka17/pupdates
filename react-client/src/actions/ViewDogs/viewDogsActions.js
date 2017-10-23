803ff65fc02eba03fa441cdb5cb92859f533d272
Tiffany Wang <twang421@gmail.com>
2017-10-23 16:41:24 -0700

    [refactor] actions for view dogs

--- react-client/src/actions/ViewDogs/viewDogsActions.js
+++ react-client/src/actions/ViewDogs/viewDogsActions.js
@@ -1,35 +1,38 @@
 import axios from 'axios';
 
-// get request for all dogs 
+// get request for all dogs for specific user
 export const getAllDogs = (userid) => {
   axios.get('http://localhost:8000/api/newdogs/' + userid)
-}
+    .then((data) => {
+      dispatch({type: 'FETCH_ALL_DOGS_FULFILLED', payload: data});
+    })
+    .catch(err => {
+      dispatch({type: 'FETCH_ALL_DOGS_REJECTED', payload: err});
+    });
+};
 
 // patch dogs to dogsSeen array
 export const updateDogsSeen = (userid, dogid) => {
   axios.patch('http://localhost:8000/api/users/seendogs/' + userid, {
     dogid: dogid
   })
-}
+  .then((data) => {
+    dispatch({type: 'UPDATE_SEEN_DOGS_FULFILLED', payload: data});
+  })
+  .catch(err => {
+    dispatch({type: 'UPDATE_SEEN_DOGS_REJECTED', payload: err});
+  });
+};
 
-
-
-
-
-
-
-
-
-
-export function getDog() {
-  return function(dispatch) {
-    axios.get('http://localhost:8000/api/dogs/')
-      .then(({ data }) => {
-        console.log(data);
-        dispatch({type: 'FETCH_ALL_DOGS_FULFILLED', payload: data});
-      })
-      .catch(err => { 
-        dispatch({type: 'FETCH_ALL_DOGS_REJECTED', payload: err});
-      });
-  };
+// patch dogs to dogsLiked array
+export const updateLikedDogs = (userid, dogid) => {
+  axios.patch('http://localhost:8000/api/users/likeddogs/' + userid, {
+    dogid: dogid
+  })
+  .then((data) => {
+    dispatch({type: 'UPDATE_LIKED_DOGS_FULFILLED', payload: data});
+  })
+  .catch(err => {
+    dispatc({type: 'UPDATE_LIKED_DOGS_REJECTED', payload: err});
+  });
 };
