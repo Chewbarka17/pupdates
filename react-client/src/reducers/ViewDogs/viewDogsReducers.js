a07c4add0b20df02cac774f90155c768a2fd529f
Tiffany Wang <twang421@gmail.com>
2017-10-25 09:36:56 -0700

    [refactor] viewDogsReducers case name

--- react-client/src/reducers/ViewDogs/viewDogsReducers.js
+++ react-client/src/reducers/ViewDogs/viewDogsReducers.js
@@ -3,28 +3,32 @@ import action from '../../actions/ViewDogs/viewDogsActions';
 const initialState = {
   unseenDogs: [],
   likedDogs: [],
+  seenDogs: [],
   fetched: false,
-  updated: false,
+  seenUpdated: false,
+  likedUpdated: false,
   error: null
 }
 
 const viewDogReducer = (state=initialState, action) => {
   switch(action.type) {
-    case 'FETCH_ALL_DOGS_FULFILLED': {
+    case 'FETCH_ALL_UNSEEN_DOGS_FULFILLED': {
       return Object.assign({}, state, {
-
+        fetched: true,
+        unseenDogs: action.payload // [{dog}, {dog}, {dog},...]
       }); 
     }
 
-    case 'FETCH_ALL_DOGS_REJECTED': {
+    case 'FETCH_ALL_UNSEEN_DOGS_REJECTED': {
       return Object.assign({}, state, {
         error: action.payload
       });
     }
 
-    case 'UPDATE_SEEN_DOGS_FULFILLED': {
+    case 'UPDATE_SEEN_DOGS_FULFILLED': { // adds a dog to dogsSeen
       return Object.assign({}, state, {
-        
+        seenUpdated: true,
+        seenDogs: action.payload // dogsSeen: ["1234dogid56789", "98765dogid56789"]
       });
     }
 
@@ -34,9 +38,10 @@ const viewDogReducer = (state=initialState, action) => {
       });
     }
 
-    case 'UPDATE_LIKED_DOGS_FULFILLED': {
+    case 'UPDATE_LIKED_DOGS_FULFILLED': { // adds a dog to dogsLiked
       return Object.assign({}, state, {
-        
+        likedUpdated: true,
+        likedDogs: action.payload // dogsLiked: ["1234dogid56789", "98765dogid56789"]
       });
     }
 
