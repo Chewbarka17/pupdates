ae3096ee38df03fe64b8f8f57db7e032fbc8d858
Tiffany Wang <twang421@gmail.com>
2017-10-25 14:24:53 -0700

    [remove] commented out lines

--- react-client/src/reducers/ViewDogs/viewDogsReducers.js
+++ react-client/src/reducers/ViewDogs/viewDogsReducers.js
@@ -13,45 +13,43 @@ const initialState = {
 const viewDogReducer = (state=initialState, action) => {
   switch(action.type) {
     case 'FETCH_ALL_UNSEEN_DOGS_FULFILLED': {
-      console.log('Im in the reducers');
       return Object.assign({}, state, {
         fetched: true,
-        unseenDogs: action.payload // [{dog}, {dog}, {dog},...]
+        unseenDogs: action.payload
       }); 
     }
 
     case 'FETCH_ALL_UNSEEN_DOGS_REJECTED': {
-      console.log('Im in the reducers');
       return Object.assign({}, state, {
         error: action.payload
       });
     }
 
-    // case 'UPDATE_SEEN_DOGS_FULFILLED': { // adds a dog to dogsSeen
-    //   return Object.assign({}, state, {
-    //     seenUpdated: true,
-    //     seenDogs: action.payload // dogsSeen: ["1234dogid56789", "98765dogid56789"]
-    //   });
-    // }
+    case 'UPDATE_SEEN_DOGS_FULFILLED': {
+      return Object.assign({}, state, {
+        seenUpdated: true,
+        seenDogs: action.payload
+      });
+    }
 
-    // case 'UPDATE_SEEN_DOGS_REJECTED': {
-    //   return Object.assign({}, state, {
-    //     error: action.payload
-    //   });
-    // }
+    case 'UPDATE_SEEN_DOGS_REJECTED': {
+      return Object.assign({}, state, {
+        error: action.payload
+      });
+    }
 
-    // case 'UPDATE_LIKED_DOGS_FULFILLED': { // adds a dog to dogsLiked
-    //   return Object.assign({}, state, {
-    //     likedUpdated: true,
-    //     likedDogs: action.payload // dogsLiked: ["1234dogid56789", "98765dogid56789"]
-    //   });
-    // }
+    case 'UPDATE_LIKED_DOGS_FULFILLED': {
+      return Object.assign({}, state, {
+        likedUpdated: true,
+        likedDogs: action.payload
+      });
+    }
 
-    // case 'UPDATE_LIKED_DOGS_REJECTED': {
-    //   return Object.assign({}, state, {
-    //     error: action.payload
-    //   });
-    // }
+    case 'UPDATE_LIKED_DOGS_REJECTED': {
+      return Object.assign({}, state, {
+        error: action.payload
+      });
+    }
   }
   return state;
 }
