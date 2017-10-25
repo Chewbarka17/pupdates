f5d0391e0871a16389db115bf79922e627223805
Tiffan Phan <thetiffanyphan@gmail.com>
2017-10-24 19:43:28 -0700

    [add] redux persists

--- react-client/Store/Store.js
+++ react-client/Store/Store.js
@@ -1,11 +1,25 @@
 import { applyMiddleware, createStore } from 'redux';
-
+import { persistStore, persistReducer } from 'redux-persist';
 import promise from 'redux-promise-middleware';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import storage from 'redux-persist/es/storage';
 
-import reducer from '../src/reducers/index';
+import rootReducer from '../src/reducers/index';
 
+const config = {
+  key: 'root', // key is required
+  storage, // storage is now required
+};
+
+const reducer = persistReducer(config, rootReducer);
 const middleware = applyMiddleware(promise(), thunk, createLogger());
 
-export default createStore(reducer, middleware);
+function configureStore() {
+  const store = createStore(reducer, middleware);
+  const persistor = persistStore(store);
+
+  return { persistor, store };
+}
+
+export default configureStore;
