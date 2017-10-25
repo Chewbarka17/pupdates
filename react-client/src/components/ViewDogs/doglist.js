e1a48a2b12149fc8ede8dc94bbc60422c8ec7cf7
Tiffany Wang <twang421@gmail.com>
2017-10-25 14:26:11 -0700

    [refactor] dogList to render data correctly

--- react-client/src/components/ViewDogs/doglist.js
+++ react-client/src/components/ViewDogs/doglist.js
@@ -1,3 +1,8 @@
+// TODO:
+// calculate # of miles away
+// click on dog's pic to view their profile
+// render userid dynamically ("59e570f1e46ed4333725a612")
+
 import React, { Component } from 'react';
 import {
   StyleSheet,
@@ -15,35 +20,6 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import * as viewDogActions from '../../actions/ViewDogs/viewDogsActions';
-
-// each card should have:
-// dog's name, # of miles away
-// click on dog's pic to view their profile
-
-// dummy data ====================================
-var image1 = require('./corgi.jpg');
-var image2 = require('./samoyed.png');
-var image3 = require('./shiba.jpg');
-
-const Cards = [
-{
-  "id": 1,
-  "first_name": "Happy",
-  "distance": 38,
-  "image": image1
-}, {
-  "id": 2,
-  "first_name": "Snuffles",
-  "distance": 49,
-  "image": image2
-}, {
-  "id": 3,
-  "first_name": "Rover",
-  "distance": 39,
-  "image": image3
-}];
-// dummy data ====================================
-
 
 class ViewDogsScreen extends React.Component {
   static navigationOptions = {
@@ -64,49 +40,23 @@ class ViewDogsScreen extends React.Component {
     super(props);
 
     this.state = {
-      cards: Cards,
-      unseenDogs: [],
-      currentDog: [],
       error: null
     }
   }
 
   componentDidMount() {
-    console.log("this.props: ", this.props);
-    console.log("this.props.actions: ", this.props.actions); // 3 action functions
-    console.log("this.props.navigation.state: ", this.props.navigation.state);
-
-    // call getAllUnseenDogs action; this.props.actions.getAllUnseenDogs(userid)
     this.props.actions.getAllUnseenDogs("59e570f1e46ed4333725a612");
-    console.log("this.props.navigation.state.unseenDogs: ", this.props.navigation.state.unseenDogs); // expecting: [{dog1}, {dog2}, {dog3}]
-
-    //this.getUnseenDogs();
-
+    console.log("this.props.viewDogs.unseenDogs", this.props.viewDogs.unseenDogs);
   }
-
-  // setStateOfCurrentDog = (x) => {
-  //   this.setState({ currentDog: x }, () => {
-  //     console.log("this.state.currentDog: ", this.state.currentDog)
-      
-  //   })
-  // }
-
-  // getUnseenDogs = () => {
-  //   axios.get('http://localhost:8000/api/newdogs/' + '59e570f1e46ed4333725a612')
-  //   .then(({ data }) => {
-  //     this.setState({ unseenDogs: data }, () => {
-  //       console.log("this.state.unseenDogs after get request: ", this.state.unseenDogs); // [{dog1}, {dog2},...]
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.log('failed to get unseen dogs: ', err)
-  //   });
-  // }
 
   Card(x) {
     return (
       <View style={styles.card}>
-        <Image source ={{uri: x.pictures[0]}} resizeMode="contain" style ={{width:350, height:350}} />
+        <Image
+          source ={{uri: x.pictures[0]}}
+          resizeMode="contain"
+          style ={{width:350, height:350}}
+        />
         <View style={{width:350, height:70, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
           <View style={{flexDirection:'row', margin:15, marginTop:25,}} >
             <Text style={{fontSize:20, fontWeight:'300', color:'#444'}}>{x.name} </Text>
@@ -119,19 +69,36 @@ class ViewDogsScreen extends React.Component {
     )
   }
   
-  handleYup(card) {
-    console.log(`Yup for ${card.text}`); // undefined
-    
-    // call updateDogsSeen action; this.props.actions.updateDogsSeen(userid, dogid) how to grab dogid of the card that's being swiped?
-
-
-    // call updateLikedDogs action; this.props.actions.updateLikedDogs(userid, dogid)
+  // swipe cards
+  handleYup(cardData) {
+    this.props.actions.updateDogsSeen("59e570f1e46ed4333725a612", cardData._id);
+    this.props.actions.updateLikedDogs("59e570f1e46ed4333725a612", cardData._id);
+    console.log("swipe yup dog id: ", cardData._id);
   }
 
-  handleNope(card) {
-    console.log(`Nope for ${card.text}`); // undefined
-    
-    // call updateDogsSeen action; this.props.actions.updateDogsSeen(userid, dogid)
+  handleNope(cardData) {
+    this.props.actions.updateDogsSeen("59e570f1e46ed4333725a612", cardData._id);
+    console.log("swipe nope dog id: ", cardData._id);
+  }
+
+  // press buttons
+  yup() {
+    // console.log("press yup dog arr: ", this.refs['swiper'].props.cards);
+    // console.log("press yup dog obj: ", this.refs['swiper'].props.cards[0]);
+    // console.log("press yup dogid: ", this.refs['swiper'].props.cards[0]._id);
+    this.props.actions.getAllUnseenDogs("59e570f1e46ed4333725a612");
+    this.props.actions.updateDogsSeen("59e570f1e46ed4333725a612", this.refs['swiper'].props.cards[0]._id)
+    this.props.actions.updateLikedDogs("59e570f1e46ed4333725a612", this.refs['swiper'].props.cards[0]._id)
+    this.refs['swiper']._goToNextCard();
+  }
+
+  nope() {
+    // console.log("press nope dog arr: ", this.refs['swiper'].props.cards);
+    // console.log("press nope dog obj: ", this.refs['swiper'].props.cards[0]);
+    // console.log("press nope dogid: ", this.refs['swiper'].props.cards[0]._id);
+    this.props.actions.getAllUnseenDogs("59e570f1e46ed4333725a612");
+    this.props.actions.updateDogsSeen("59e570f1e46ed4333725a612", this.refs['swiper'].props.cards[0]._id)
+    this.refs['swiper']._goToNextCard();
   }
 
   noMore() {
@@ -146,39 +113,20 @@ class ViewDogsScreen extends React.Component {
     )
   }
 
-  // buttons
-  yup(cardData) {
-    console.log(this.refs['swiper']);
-    console.log("cardData: ", cardData);
-    console.log("this.state.currentDog: ", this.state.currentDog);
-    // call updateDogsSeen action; this.props.actions.updateDogsSeen(userid, dogid) how to grab dogid of the card that's being swiped?
-
-    // call updateLikedDogs action; this.props.actions.updateLikedDogs(userid, dogid)
-
-    this.refs['swiper']._goToNextCard();
-  }
-
-  nope() {
-    console.log(this.refs['swiper']);
-    // call updateDogsSeen action; this.props.actions.updateDogsSeen(userid, dogid)
-
-    this.refs['swiper']._goToNextCard();
-  }
-
   render() {
     return (
       <View style={styles.container}>
         <SwipeCards
           ref = {'swiper'}
-          cards={this.state.unseenDogs}
+          cards={this.props.viewDogs.unseenDogs}
           containerStyle = {{  backgroundColor: '#f7f7f7', alignItems:'center', margin:20}}
           renderCard={(cardData) => (this.Card(cardData))}
+          handleYup={(cardData) => (this.handleYup(cardData))}
+          handleNope={(cardData) => (this.handleNope(cardData))}
           renderNoMoreCards={() => this.noMore()}
-          handleYup={this.handleYup}
-          handleNope={this.handleNope} 
         />
         <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-          <TouchableOpacity 
+          <TouchableOpacity
             style = {styles.buttons}
             onPress = {() => this.nope()}
           >
@@ -251,7 +199,7 @@ const styles = StyleSheet.create({
 
 const viewDogsState = (store) => {
   return {
-    viewDogs: store.ViewDogs.seenDogs
+    viewDogs: store.ViewDogs
   }
 }
 
