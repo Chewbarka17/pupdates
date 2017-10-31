f98cdcc41f21f1bceb0bf0826fb735c5dbeb5781
ajliss <aaron@lissismore.com>
2017-10-31 09:30:27 -0700

    [add] more appropriate cases for chat room reducers.

--- react-client/src/reducers/MessageReducers/ChatRoomReducers.js
+++ react-client/src/reducers/MessageReducers/ChatRoomReducers.js
@@ -5,19 +5,19 @@ const initialState = {
 
 const chatReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'MESSAGE_CHANGE': {
-      // console.log('Messages reducer payload, ', action.payload);
-      return Object.assign({}, state, {
-        // log: action.payload,
-      });
-    }
     case 'POST_ROOM_FULFILLED': {
       // console.log('Messages reducer payload, ', action.payload);
       return Object.assign({}, state, {
         rooms: state.rooms.concat(action.payload),
       });
     }
-    case 'SAVE_ROOMS': {
+    case 'POST_ROOM_REJCECTED': {
+      // console.log('Messages reducer payload, ', action.payload);
+      return Object.assign({}, state, {
+        error: action.payload,
+      });
+    }
+    case 'GET_ROOMS_ FULFILLED': {
       return Object.assign({}, state, {
         rooms: action.payload,
       });
