f017ab76ad8ab11584e62680ed98f4a7bc507e44
ajliss <aaron@lissismore.com>
2017-10-31 09:27:46 -0700

    [remove] console.logs from messages controller.

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -8,7 +8,6 @@ module.exports = {
   getMessages: (req, res) => {
     Rooms.find({ _id: req.params.roomid }, (err) => {
       if (err) {
-        console.log('Error getting messages', err);
         res.status(500).send(err);
       }
     })
@@ -32,7 +31,6 @@ module.exports = {
       },
     }, { new: true }, (err, data) => {
       if (err) {
-        console.log('update error', err);
         res.status(500).send('error', err);
       }
       res.status(201).send(data);
@@ -43,9 +41,9 @@ module.exports = {
     req.body.uids.forEach((uid) => {
       Owners.findOneAndUpdate({ _id: uid },
         { $pull: { chatRooms: req.params.roomid } },
-        (err, data) => {
+        (err) => {
           if (err) {
-            console.log('error1', err)
+            res.status(500).send(err);
           }
           Owners.findOneAndUpdate({ _id: uid },
             {
@@ -58,7 +56,7 @@ module.exports = {
             },
             (error) => {
               if (error) {
-                console.log('error2', error);
+                res.status(500).send(error);
               }
             });
         });
@@ -66,9 +64,7 @@ module.exports = {
     res.status(201).send('yay');
   },
 
-  // I'm putting the names and user ids of the two people involved in the conversation in an array. Will update logic when front end is up.
   createRoom: (req, res) => {
-
     Owners.find({ _id: req.body.uids }, (err) => {
       if (err) {
         res.status(500).send(err);
@@ -81,7 +77,7 @@ module.exports = {
         if (roomid.length > 0) {
           Rooms.findOne({ _id: roomid }, (err) => {
             if (err) {
-              console.log(err);
+              res.status(500).send(err);              
             }
           })
             .then((room) => {
@@ -97,18 +93,14 @@ module.exports = {
             messages: [],
           });
           room.save((err) => {
-            console.log('room saved')
             if (err) {
-              console.error('Could not save owner', err);
+              res.status(500).send(err);
             }
           })
             .then((result) => {
-              console.log('.then', result);
               result.uids.forEach((uid) => {
-                console.log(uid)
                 Owners.findOneAndUpdate({ _id: uid }, { $push: { chatRooms: result._id } }, (err) => {
                   if (err) {
-                    console.log('add dog update error', err);
                     res.status(500).send('error', err);
                   }
                 });
@@ -125,28 +117,26 @@ module.exports = {
   getRooms: (req, res) => {
     Owners.find({ _id: req.params.userid }, (err) => {
       if (err) {
-        console.log('Error getting messages', err);
         res.status(500).send(err);
       }
     })
       .then((data) => {
         Rooms.find({ _id: { $in: data[0].chatRooms } }, (err) => {
           if (err) {
-            console.log('error getting message rooms ', err);
+            res.status(500).send(err);
           }
         })
           .then((results) => {
             const rooms = JSON.parse(JSON.stringify(results));
             const uids = [];
-            // const names = [];
 
             results.forEach((owner) => {
               const id = owner.uids.filter(uid => uid !== req.params.userid);
               uids.push(id[0]);
-            });           
+            });
             Owners.find({ _id: uids })
               .then((partners) => {
-                let refObj = {};
+                const refObj = {};
                 for (let j = 0; j < partners.length; j++) {
                   refObj[partners[j]._id] = partners[j];
                 }
@@ -155,30 +145,23 @@ module.exports = {
                   uids[k] = refObj[uids[k]];
                 }
 
-                // console.log('rooms', rooms[0])
-                // console.log('sorted', partners[0])
                 for (let i = 0; i < rooms.length; i++) {
                   console.log(uids[i].name)
                   rooms[i].partner = uids[i].name;
                 }
-                // console.log('rooms', rooms[1])
-                // console.log('_doc', rooms[1]._doc);
-                console.log('uids', uids)
-                // console.log('results', rooms[0])
+
                 rooms.sort((a, b) => {
                   return new Date(b.messages[0].createdAt) - new Date(a.messages[0].createdAt);
                 });
-                // console.log('rooms', rooms[0])
+
                 res.status(200).send(rooms);
               });
           })
           .catch((err) => {
-            console.log('Error getting message rooms', err);
             res.status(500).send(err);
           });
       })
       .catch((err) => {
-        console.log('Error getting user', err);
         res.status(500).send(err);
       });
   },
