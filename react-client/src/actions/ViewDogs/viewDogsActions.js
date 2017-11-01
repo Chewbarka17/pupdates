de3fd9852a21de626f19b2e649b4b0851feed20a
Tiffan Phan <thetiffanyphan@gmail.com>
2017-11-01 15:33:47 -0700

    [reformat] spacing for actions

--- react-client/src/actions/ViewDogs/viewDogsActions.js
+++ react-client/src/actions/ViewDogs/viewDogsActions.js
@@ -1,35 +1,36 @@
 import axios from 'axios';
 
-export const getAllUnseenDogs = (userid) => (dispatch) => {
-  axios.get('http://localhost:8000/api/newdogs/' + userid)
+export const getAllUnseenDogs = userid => (dispatch) => {
+  axios.get(`http://localhost:8000/api/newdogs/${userid}`)
     .then((data) => {
-      dispatch({type: 'FETCH_ALL_UNSEEN_DOGS_FULFILLED', payload: data.data});
+      dispatch({ type: 'FETCH_ALL_UNSEEN_DOGS_FULFILLED', payload: data.data });
     })
-    .catch(err => {
-      dispatch({type: 'FETCH_ALL_UNSEEN_DOGS_REJECTED', payload: err});
+    .catch((err) => {
+      dispatch({ type: 'FETCH_ALL_UNSEEN_DOGS_REJECTED', payload: err });
     });
 };
 
 export const updateDogsSeen = (userid, dogid) => (dispatch) => {
-  axios.patch('http://localhost:8000/api/users/seendogs/' + userid, {
+  axios.patch(`http://localhost:8000/api/users/seendogs/${userid}`, {
     dogid,
   })
-  .then((data) => {
-    dispatch({ type: 'UPDATE_SEEN_DOGS_FULFILLED', payload: data[0].dogsSeen });
-  })
-  .catch(err => {
-    dispatch({type: 'UPDATE_SEEN_DOGS_REJECTED', payload: err});
-  });
+    .then(() => {
+      // console.log('what is data in actions, ', data);
+      dispatch({ type: 'UPDATE_SEEN_DOGS_FULFILLED', payload: dogid });
+    })
+    .catch((err) => {
+      dispatch({ type: 'UPDATE_SEEN_DOGS_REJECTED', payload: err });
+    });
 };
 
 export const updateLikedDogs = (userid, dogid) => (dispatch) => {
-  axios.patch('http://localhost:8000/api/users/likeddogs/' + userid, {
-    dogid: dogid
+  axios.patch(`http://localhost:8000/api/users/likeddogs/${userid}`, {
+    dogid,
   })
-  .then((data) => {
-    dispatch({type: 'UPDATE_LIKED_DOGS_FULFILLED', payload: data[0].dogsLiked});
-  })
-  .catch(err => {
-    dispatch({type: 'UPDATE_LIKED_DOGS_REJECTED', payload: err});
-  });
+    .then(() => {
+      dispatch({ type: 'UPDATE_LIKED_DOGS_FULFILLED', payload: dogid });
+    })
+    .catch((err) => {
+      dispatch({ type: 'UPDATE_LIKED_DOGS_REJECTED', payload: err });
+    });
 };
