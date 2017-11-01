e094a728c5ad8bb2674a56b97ecc2f82081eccb1
Tiffan Phan <thetiffanyphan@gmail.com>
2017-11-01 15:36:02 -0700

    [update] redux stores for liked and seen dogs

--- react-client/src/reducers/ViewDogs/viewDogsReducers.js
+++ react-client/src/reducers/ViewDogs/viewDogsReducers.js
@@ -2,56 +2,57 @@ import action from '../../actions/ViewDogs/viewDogsActions';
 
 const initialState = {
   unseenDogs: [],
-  likedDogs: [],
-  seenDogs: [],
-  fetched: false,
-  seenUpdated: false,
-  likedUpdated: false,
-  error: null
-}
+  // likedDogs: [],
+  // seenDogs: [],
+  // fetched: false,
+  // seenUpdated: false,
+  // likedUpdated: false,
+  error: null,
+};
 
-const viewDogReducer = (state=initialState, action) => {
-  switch(action.type) {
+const viewDogReducer = (state = initialState, action) => {
+  switch (action.type) {
     case 'FETCH_ALL_UNSEEN_DOGS_FULFILLED': {
       return Object.assign({}, state, {
-        fetched: true,
-        unseenDogs: action.payload
-      }); 
+        // fetched: true,
+        unseenDogs: action.payload,
+      });
     }
 
     case 'FETCH_ALL_UNSEEN_DOGS_REJECTED': {
       return Object.assign({}, state, {
-        error: action.payload
+        error: action.payload,
       });
     }
 
     case 'UPDATE_SEEN_DOGS_FULFILLED': {
       return Object.assign({}, state, {
-        seenUpdated: true,
-        seenDogs: action.payload
+        // seenUpdated: true,
+        unseenDogs: state.unseenDogs.filter(dog => dog._id !== action.payload),
       });
     }
 
     case 'UPDATE_SEEN_DOGS_REJECTED': {
       return Object.assign({}, state, {
-        error: action.payload
+        error: action.payload,
       });
     }
 
     case 'UPDATE_LIKED_DOGS_FULFILLED': {
       return Object.assign({}, state, {
-        likedUpdated: true,
-        likedDogs: action.payload
+        // likedUpdated: true,
+        unseenDogs: state.unseenDogs.filter(dog => dog._id !== action.payload),
       });
     }
 
     case 'UPDATE_LIKED_DOGS_REJECTED': {
       return Object.assign({}, state, {
-        error: action.payload
+        error: action.payload,
       });
     }
+    default:
+      return state;
   }
-  return state;
-}
+};
 
-export default viewDogReducer; 
+export default viewDogReducer;
