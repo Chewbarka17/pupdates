7e265c471d31f73a87b9cf434098c251d61e1efb
Audiodrome <lien.daniel.h@gmail.com>
2017-11-01 20:09:05 -0700

    [Update] Logout uses navigation stack reset.

--- react-client/src/components/Authentication/Logout/logout.js
+++ react-client/src/components/Authentication/Logout/logout.js
@@ -4,7 +4,7 @@ import {
   View
 } from 'react-native';
 import React, { Component } from 'react';
-import { StackNavigator} from 'react-navigation';
+import { NavigationActions } from 'react-navigation';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import SplashPageView from '../../Splash/splashPageView';
 
@@ -24,6 +24,8 @@ class LogoutScreen extends React.Component {
   // }
   constructor(props) {
     super(props);
+
+    this.navigateToHome = this.navigateToHome.bind(this);
   }
   
   componentDidMount() {
@@ -33,12 +35,22 @@ class LogoutScreen extends React.Component {
       let accessToken = data.accessToken;
       if (accessToken !== null) {
         LoginManager.logOut();
-        this.props.navigation.navigate('Home');
+        this.navigateToHome();
       }
     })
     .catch(error => {
-      this.props.navigation.navigate('Home');
+      this.navigateToHome();
     });
+  }
+
+  navigateToHome() {
+    const navigateToHome = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({routeName: 'Home'})
+      ]
+    });
+    this.props.navigation.dispatch(navigateToHome);
   }
 
   render() {
