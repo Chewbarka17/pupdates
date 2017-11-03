4e2c75aee90ed3b9496fe087ee6bfc315738327f
Tiffany Wang <twang421@gmail.com>
2017-11-03 15:06:30 -0700

    [add] styling

--- react-client/src/components/Profiles/viewOwnerProfile.js
+++ react-client/src/components/Profiles/viewOwnerProfile.js
@@ -128,18 +128,14 @@ class viewOwnerProfile extends Component {
           </Button>
         </View>
         <View style={[styles.boxContainer, styles.boxFour]}>
-         { user.age ?
-         <Text style={styles.baseText}>
-         </Text> : null
-         }
+        <Text style={[styles.baseText, styles.location]}>
+           {user.location.split(',')[1]}
+         </Text>
          { user.bio ?
          <Text style={styles.baseText}>
            {user.bio}
          </Text> : null
          }
-         <Text style={styles.baseText}>
-           {user.location.split(',')[1]}
-         </Text>
         </View>
         <View style={[styles.boxContainer, styles.boxFive]}>
            <FlatList
@@ -250,6 +246,10 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     color: '#3f3f3f',
+  },
+  location: {
+    color: '#898989',
+    fontWeight: 'bold',
   },
 });
 
